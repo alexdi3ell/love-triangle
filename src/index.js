32938f2acd4c37ccc458a8560a85b4550978cae5
Alex Di3ell <a.n.degilevich@gmail.com>
2019-02-22 15:46:28 +0300

    Solution Love triangles 1 commit

--- src/index.js
+++ src/index.js
@@ -4,4 +4,20 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let arr = preferences;
+  let solution =0;
+  for ( let i = 0; i < arr.length; i++ ) {
+    //Spichonee A
+    let valOne = arr[i];
+    //Spichonee B
+    let valTwo = arr[valOne-1];
+    //Spichonee C
+    let valTree = arr[valTwo-1];
+    if (valTree === i+1 && arr[i]!=i+1){
+      solution++;
+      arr[i],arr[valOne-1],arr[valTwo-1]=-1
+    }  
 };
+return solution;
+}
+
